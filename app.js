94d773154c3ba3f6d278824223a892a6d522103e
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2021-02-22 21:04:22 +0600

    repeating application implemented

---
+++ app.js
@@ -0,0 +1,25 @@
+class drumKit{
+    constructor(){
+        this.pads = document.querySelectorAll('.pad')
+        this.kickAudio = document.querySelector('kick-sound')
+        this.snareAudio = document.querySelector('snare-sound')
+        this.hihatAudio = document.querySelector('hihat-sound')
+        this.index = 0
+
+    }
+
+    repeat(){
+        let steps = this.index % 8
+        this.index++
+    }
+    start(){ 
+        setInterval(() => {
+            this.repeat()
+        }, 1000)
+    }
+}
+
+
+const kit = new drumKit()
+
+kit.start()
