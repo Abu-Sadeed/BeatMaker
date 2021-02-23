49629926c069d60638bb4b63db0f2303a67b8f45
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2021-02-23 10:38:47 +0600

    audible sounds from pattern

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 class drumKit{
     constructor(){
         this.pads = document.querySelectorAll('.pad')
-        this.kickAudio = document.querySelector('kick-sound')
-        this.snareAudio = document.querySelector('snare-sound')
-        this.hihatAudio = document.querySelector('hihat-sound')
+        this.playBtn = document.querySelector('.play')
+        this.kickAudio = document.querySelector('.kick-sound')
+        this.snareAudio = document.querySelector('.snare-sound')
+        this.hihatAudio = document.querySelector('.hihat-sound')
         this.index = 0
+        this.bpm = 150
+    }
 
+    activePad(){
+        this.classList.toggle('active')
     }
 
     repeat(){
         let steps = this.index % 8
+        const activeBars = document.querySelectorAll(`.b${steps}`)
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`
+            if(bar.classList.contains('active')){
+                if(bar.classList.contains('kick-pad')){
+                    this.kickAudio.play()
+                }
+                if(bar.classList.contains('snare-pad')){
+                    this.snareAudio.play()
+                }
+                if(bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.play()
+                }
+            }
+        })
         this.index++
     }
-    start(){ 
+    start(){
+        const interval = (60/this.bpm) * 1000 
         setInterval(() => {
             this.repeat()
-        }, 1000)
+        }, interval)
     }
 }
 
 
 const kit = new drumKit()
 
-kit.start()
+kit.pads.forEach(pad => {
+    pad.addEventListener('click', kit.activePad)
+    pad.addEventListener('animationend', function(){
+        this.style.animation = ''
+    })
+})
+
+kit.playBtn.addEventListener('click', () => {
+    kit.start()
+})
